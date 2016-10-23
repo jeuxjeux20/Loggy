7b5547114dd48bd266e3634e63731768d9a3eb51
jeuxjeux20 <jeuxjeux20@outlook.fr>
2016-10-23 14:06:36 +0200

    v2

--- Loggy/Program.cs
+++ Loggy/Program.cs
@@ -5,12 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using System.IO;
 namespace Loggy
 {
     class Program
     {
         static void Main(string[] args) => new Program().Start(args);
-
+        #region Accept
+        private bool isAcceptable(CommandEventArgs e)
+        {
+            bool acc = false;
+            foreach (var role in e.User.Roles)
+            {
+                if (role.Name.ToLower().Contains("logger") || role.Name.ToLower().Contains("Admin"))
+                {
+                    acc = true;
+                    break;
+                }
+            }
+            return acc;
+        }
+        private bool isAcceptable(User e)
+        {
+            bool acc = false;
+            foreach (var role in e.Roles)
+            {
+                if (role.Name.ToLower().Contains("logger") || role.Name.ToLower().Contains("Admin"))
+                {
+                    acc = true;
+                    break;
+                }
+            }
+            return acc;
+        }
+        #endregion
+        
         private DiscordClient _client;
         private bool safeTo(Dictionary<Channel, Channel> dict, Pair<Channel, Channel> per)
         {
@@ -40,18 +69,101 @@ namespace Loggy
                 .Description("Make a list of all loggers")
                 .Do(async e =>
                     {
-                        try
+                        bool nothing = true;
+                        string message = "List of records :";
+                        foreach (var item in toRecord)
                         {
-                            foreach (var item in toRecord)
-                            {
-
-                            }
+                            nothing = false;
+                            message += $"{Environment.NewLine} {item.Key} ---> {item.Value}";
                         }
-                        catch (Exception)
-                        {
+
+                        if (nothing)
                             await e.Channel.SendMessage("There isn't any loggers ! Create one using &record");
+                        else
+                        {
+                            await e.Channel.SendMessage(message);
                         }
                     });
+            
+
+            _client.GetService<CommandService>().CreateCommand("delete")
+.Description("Delete a record")
+.Parameter("a", ParameterType.Required)
+.Parameter("b", ParameterType.Required)
+.Do(async e =>
+    {
+        string a = e.GetArg("a");
+        string b = e.GetArg("b");
+        Pair<bool, bool> foundie = new Pair<bool, bool>(false, false); // topkek
+        var toRecordTemp = new Pair<Channel, Channel>(null, null);
+
+        foreach (var item in e.Server.AllChannels)
+        {
+
+            if (a == b)
+            {
+                await e.Channel.SendMessage("no really, silly guy xd");
+                break;
+            }
+            if (item.Name == a)
+            {
+                foundie.First = true;
+                toRecordTemp.First = item;
+            }
+            if (item.Name == b)
+            {
+                foundie.Second = true;
+                toRecordTemp.Second = item;
+            }
+        }
+        if (isAcceptable(e) && foundie.First && foundie.Second)
+        {
+            foreach (var item in toRecord.ToList())
+            {
+                if (item.Key == toRecordTemp.First && item.Value == toRecordTemp.Second)
+                {
+                    toRecord.Remove(item.Key);
+                }
+            }
+            await e.Channel.SendMessage($"Sucessfully deleted record ! ({toRecordTemp.First} ---> {toRecordTemp.Second})");
+        }
+    });
+
+
+            _client.GetService<CommandService>().CreateCommand("disconnect")
+            .Description("Disconnects the bot, what else")
+            .Do(async e =>
+             {
+                 if (e.User.Name == "jeuxjeux20#4664")
+                 {
+                     await e.Channel.SendMessage("Bye :frowning:");
+                     await Task.Delay(1000);
+                     await _client.Disconnect();
+                     _client.Dispose();
+                 }
+                 else
+                 {
+                     await e.Channel.SendMessage("Only jeuxjeux20 can disconnect the bot :p");
+                 }
+             });
+
+            _client.GetService<CommandService>().CreateCommand("invite")
+.Description("Get an invite link kek")
+.Do(async e =>
+    {
+        await e.Channel.SendMessage(@"Here is the link : https://discordapp.com/oauth2/authorize?client_id=239326847433703424&scope=bot&permissions=0");
+    });
+
+
+            _client.GetService<CommandService>().CreateCommand("about")
+        .Description("About the dev c:")
+        .Do(async e =>
+            {
+                await e.Channel.SendMessage
+                ($@"```This bot is made be jeuxjeux20,
+                    it is currently on {_client.Servers.Count()} server(s)
+                    I hope that you like it c:```");
+            });
 
 
             _client.GetService<CommandService>().CreateCommand("record")
@@ -64,15 +176,7 @@ namespace Loggy
                  string b = e.GetArg("b");
                  Pair<bool, bool> foundie = new Pair<bool, bool>(false, false); // topkek
                  var toRecordTemp = new Pair<Channel, Channel>(null, null);
-                 bool isAcceptable = false;
-                 foreach (var role in e.User.Roles)
-                 {
-                     if (role.Name.ToLower().Contains("logger") || role.Name.ToLower().Contains("Admin"))
-                     {
-                         isAcceptable = true;
-                         break;
-                     }
-                 }
+
                  foreach (var item in e.Server.AllChannels)
                  {
 
@@ -92,16 +196,29 @@ namespace Loggy
                          toRecordTemp.Second = item;
                      }
                  }
-                 if (foundie.First == false || foundie.Second == false && isAcceptable && safeTo(toRecord, toRecordTemp))
+                 if (foundie.First == false || foundie.Second == false && isAcceptable(e))
                  {
                      await e.Channel.SendMessage($"Something didn't got right - Listening : {foundie.First} ; Target : {foundie.Second}");
                  }
-                 else if (isAcceptable && safeTo(toRecord, toRecordTemp))
+                 else if (isAcceptable(e) && safeTo(toRecord, toRecordTemp))
                  {
                      await e.Channel.SendMessage($"~~Succesfully installed Windows 10~~ Succesfully recording #{a} to output #{b}");
-                     toRecord.Add(toRecordTemp.First, toRecordTemp.Second);
+                     bool cool = true;
+                     foreach (var item in toRecord.ToList())
+                     {
+                         if (item.Key == toRecordTemp.First)
+                         {
+                             await e.Channel.SendMessage("A record is alerady targeting this channel, replacing...");
+                             cool = false;
+                             toRecord[item.Key] = toRecordTemp.Second;
+                             break;
+                         }
+
+                     }
+                     if (cool)
+                         toRecord.Add(toRecordTemp.First, toRecordTemp.Second);
                  }
-                 else if (!isAcceptable)
+                 else if (!isAcceptable(e))
                  {
                      await e.Channel.SendMessage($"Sorry, but you aren't allowed to use this command ; please get a role that contains \"logger\" or \"Admin\" in its name");
                  }
@@ -125,10 +242,52 @@ namespace Loggy
 
             _client.ExecuteAndWait(async () =>
             {
+                string tok;
+                string path = AppDomain.CurrentDomain.BaseDirectory + "toke.t";
+
+                string kek;
                 if (args == null || (args.Length > 0 ? args?[0] == "" : true))
-                    args = new string[1] { "MjM5MzI2ODQ3NDMzNzAzNDI0.CuzKTg.c8nGLDNs-RLPVOVX9DWD4eyoH20" };
-                await _client.Connect(args[0], TokenType.Bot);
+                {
+                    tok = "kek";
+                    using (FileStream file = File.Open(AppDomain.CurrentDomain.BaseDirectory + "toke.t", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+                    {
+                        using (var str = new StreamReader(file))
+                        {
+                            tok = str.ReadToEnd();
+                            str.Close();
+                        }
+                    }
+
+                }
+                else
+                {
+                    tok = args[0];
+                }
+                Retry:
+                try
+                {
+                    await _client.Connect(tok, TokenType.Bot);
+                }
+                catch (Exception)
+                {
+                    await Console.Out.WriteLineAsync("Insert token plesssss");
+                    await Console.Out.FlushAsync();
+                    kek = await Console.In.ReadLineAsync();
+                    tok = kek;
+                    goto Retry;
+                }
                 await Console.Out.WriteLineAsync("I guess i'm connected");
+                using (FileStream file = File.Open(AppDomain.CurrentDomain.BaseDirectory + "toke.t", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    using (var sw = new StreamWriter(file))
+                    {
+                        await sw.WriteLineAsync(tok);
+                        await sw.FlushAsync();
+                        sw.Close();
+                    }
+                }
+
+
             });
         }
     }
